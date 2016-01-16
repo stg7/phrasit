f7316daf4005c26feb58c2e5c163ed6e8437150e
Steve <stg7@gmx.de>
2016-01-16 18:22:24 +0100

    add blank for github project page link

--- web/links.php
+++ web/links.php
@@ -1,2 +1,8 @@
-    <li><a href="http://github.com/stg7/phrasit" ><img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" style="height:1.5em" /></a></li>
-    <li><a href="./about.php" >about</a></li>
+    <li>
+        <a href="http://github.com/stg7/phrasit" target="_blank">
+        <img src="https://assets-cdn.github.com/images/modules/logos_page/GitHub-Mark.png" style="height:1.5em" />
+        </a>
+    </li>
+    <li>
+        <a href="./about.php" >about</a>
+    </li>
