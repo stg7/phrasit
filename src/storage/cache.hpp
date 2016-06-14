457a90c170a5c36e83aaf7f84a98c73cd937a4c0
Steve <stg7@gmx.de>
2016-06-14 03:10:34 +0200

    add serialization/deserialisation of cache

--- src/storage/cache.hpp
+++ src/storage/cache.hpp
@@ -38,7 +38,6 @@
 #include <cereal/types/map.hpp>
 #include <cereal/types/vector.hpp>
 #include <cereal/types/string.hpp>
-#include <cereal/archives/json.hpp>
 #include <cereal/archives/binary.hpp>
 
 #include "utils/log.hpp"
