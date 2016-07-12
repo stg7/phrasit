3f697a6795764219565cd3e6f45878aaea7ee2d6
Steve <stg7@gmx.de>
2016-07-13 00:54:37 +0200

    add error handling for convert script

--- datasets/convert.sh
+++ datasets/convert.sh
@@ -22,5 +22,7 @@ infile="$1"
 if [ -f "$infile" ] ; then
     outfile="$(dirname $1)/$(basename $1 .gz).lzma"
     zcat "$infile" | lzma -9 -c -T 0 --stdout > "$outfile"
-    rm -rf "$infile"
+    if [ $? -eq 0 ]; then
+        rm -rf "$infile"
+    fi
 fi
